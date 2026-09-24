90fe9f580b2da89425c8aff124f4fea05d6244f5
Vanello1908 <vanello1908@gmail.com>
2026-09-24 10:09:39 +0300

    Fix typo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", (event) => {
-  const zoomLevel = Math.roundwindow.devicePixelRatio;
+  const zoomLevel = window.devicePixelRatio;
   const targetZoom = 1 / zoomLevel;
   document.body.style.zoom = `${targetZoom}`;
 });
