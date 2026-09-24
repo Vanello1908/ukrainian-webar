fb71d3ab05a72b01a06e6d536e4558b450905a92
Vanello1908 <vanello1908@gmail.com>
2026-09-24 12:29:05 +0300

    Fix resize

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-addEventListener("DOMContentLoaded", (event) => {
+function resize() {
   const zoomLevel = window.devicePixelRatio;
   const targetZoom = 1 / zoomLevel;
   document.body.style.zoom = `${targetZoom}`;
+}
 
-  const gallry_grid = document.getElementById("gallery").lastElementChild
+function load_gallry() {
+  const gallry_grid = document.getElementById("gallery").lastElementChild;
 
-  for(let i = 0; i < 9; i++) {
+  for (let i = 0; i < 9; i++) {
     const index = i;
-    let el = document.createElement("img")
-    el.src = `images/${index+1}.jpg`
+    let el = document.createElement("img");
+    el.src = `images/${index + 1}.jpg`;
     el.height = 256;
 
-    gallry_grid.appendChild(el)
+    gallry_grid.appendChild(el);
   }
+}
+
+addEventListener("DOMContentLoaded", (event) => {
+  resize();
+  load_gallry();
 });
 
+window.visualViewport.addEventListener("resize", () => {
+  resize();
+});
