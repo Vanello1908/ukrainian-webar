67e55819a8dac742e246e387a7bf77561faac3a7
Vanello1908 <vanello1908@gmail.com>
2026-09-24 10:44:08 +0300

    Fix scanner

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ addEventListener("DOMContentLoaded", (event) => {
   const zoomLevel = window.devicePixelRatio;
   const targetZoom = 1 / zoomLevel;
   document.body.style.zoom = `${targetZoom}`;
+
+  const gallry_grid = document.getElementById("gallery").lastElementChild
+
+  for(let i = 0; i < 9; i++) {
+    const index = i;
+    let el = document.createElement("img")
+    el.src = `images/${index+1}.jpg`
+    el.height = 256;
+
+    gallry_grid.appendChild(el)
+  }
 });
+
