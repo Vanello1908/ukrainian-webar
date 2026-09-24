26cbd04718c14d7f97c17bdd319d5ffeef090deb
Vanello1908 <vanello1908@gmail.com>
2026-09-24 10:07:04 +0300

    Remove rounding

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", (event) => {
-  const zoomLevel = Math.round(window.devicePixelRatio);
+  const zoomLevel = Math.roundwindow.devicePixelRatio;
   const targetZoom = 1 / zoomLevel;
   document.body.style.zoom = `${targetZoom}`;
 });
